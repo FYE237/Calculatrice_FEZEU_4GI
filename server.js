b07eb5bf21f6878849ef5055d0468e411b020998
FYE237 <yfezeu.emma@gmail.com>
2021-11-25 22:30:01 +0100

    deploiement1

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-
-    //Install express server
-    const express = require('express');
-    const path = require('path');
-
-    const app = express();
-
-    // Serve only the static files form the dist directory
-    app.use(express.static(__dirname + '/dist/fye-calco'));
-
-    app.get('/*', function(req,res) {
-        
-    res.sendFile(path.join(__dirname+'/dist/fye-calco/index.html'));
-    });
-
-    // Start the app by listening on the default Heroku port
-    app.listen(process.env.PORT || 8080);
-
+const path = require("path");
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + '/calculatrice'));
+app.get('/*', function(req,res){
+res.sendFile(path.join(__dirname, 'calculatrice', 'index.html'))
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
