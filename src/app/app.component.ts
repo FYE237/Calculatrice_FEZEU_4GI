0979489fef2ddf172bbaed4b039e34d1f3298f0f
FYE237 <yfezeu.emma@gmail.com>
2021-11-26 21:13:55 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { syntaxError } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,246 +7,80 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculatrice';
-  calcul ="";
-  op1 ="";
-  op2="";
-  operation="";
-  i=0;j=0;
-  res=0;
-
-  setOperation1(op: string){
-    if (this.getOperation1() == "SYNTAXE ERROR") this.op1="";
-    this.op1=this.op1+op;
-  }
-  setOperation2(op: string){
-    this.op2=this.op2+op;
-  }
-  getOperation1(){
-    return this.op1;
-  }
-  getOperation2(){
-    return this.op2;
+  test="True";
+  sequence="";
+  sequence1 = "";
+  res="";
+  calcul="0";
+  function(val: string) {
+    this.sequence=this.sequence+val;
   }
 
+  
+  decimal() {
+        if (this.sequence == "") this.sequence = "0.";
+      else { if (this.sequence.indexOf(".")!=-1) {
+        this.test="False";
+      }
+      this.sequence=this.sequence+".";
+    }
+}
+  
 
-  function1(){
-    if(this.operation=="")
-      {
-        this.setOperation1("1");
-        this.calcul=this.getOperation1();
-      }
-      else {
-        this.setOperation2("1");
-        this.calcul=this.getOperation2();
-      }
-      console.log(this.calcul);
+  clear() {
+    this.sequence="";
+    this.calcul="";
+    this.test="True";
+    this.sequence1="0";
+    this.res="";
   }
 
-  function2(){
-    if(this.operation=="")
-      {
-        this.setOperation1("2");
-        this.calcul=this.getOperation1();
-      }
-      else {
-        this.setOperation2("2");
-        this.calcul=this.getOperation2();
-      }
-      console.log(this.calcul);
+  egal() {
+      
+          this.res=eval(this.sequence);
+          if(this.res=="Infinity") this.calcul="MATH ERROR";
+          else this.calcul=eval(this.res);
+    //this.sequence=this.sequence;
   }
 
-  function3(){
-    if(this.operation=="")
-      {
-        this.setOperation1("3");
-        this.calcul=this.getOperation1();
-      }
-      else {
-        this.setOperation2("3");
-        this.calcul=this.getOperation2();
-      }
-      console.log(this.calcul);
+  sub() {
+    if (this.sequence.length>=1)
+    {
+      if (this.sequence[this.sequence.length-1]!="+" && this.sequence[this.sequence.length-1]!="-"&&this.sequence[this.sequence.length-1]!="/"&& this.sequence[this.sequence.length-1]!="*") this.sequence=this.sequence+"-";
+    }
+    else this.sequence="0-"; 
   }
+
+  add() {
+    if (this.sequence.length>=1)
+    {
+      if (this.sequence[this.sequence.length-1]!="+" && this.sequence[this.sequence.length-1]!="-"&&this.sequence[this.sequence.length-1]!="/"&& this.sequence[this.sequence.length-1]!="*") this.sequence=this.sequence+"+";
+    }
+    else this.sequence="0+";  
+  }
+
+  produit() {
+    if (this.sequence.length>=1)
+    {
+      if (this.sequence[this.sequence.length-1]!="+" && this.sequence[this.sequence.length-1]!="-"&&this.sequence[this.sequence.length-1]!="/"&& this.sequence[this.sequence.length-1]!="*") this.sequence=this.sequence+"*";
+    }
+    else this.sequence="0*";  
+  }   
+
+  effacer() {
+    if(this.sequence.length>=1)
+    this.sequence=this.sequence.substr(0,this.sequence.length-1);
+    else this.sequence="0";
+  }
+
+  div() {
+    if (this.sequence.length>=1)
+    {
+      if (this.sequence[this.sequence.length-1]!="+" && this.sequence[this.sequence.length-1]!="-"&&this.sequence[this.sequence.length-1]!="/"&& this.sequence[this.sequence.length-1]!="*") this.sequence=this.sequence+"/";
+    }
+    else this.sequence="0/";  
  
-  function4(){if(this.operation=="")
-  {
-    this.setOperation1("4");
-    this.calcul=this.getOperation1();
-  }
-  else {
-    this.setOperation2("4");
-    this.calcul=this.getOperation2();
-  }
-  console.log(this.calcul);
-}
-
-  function5(){if(this.operation=="")
-  {
-    this.setOperation1("5");
-    this.calcul=this.getOperation1();
-  }
-  else {
-    this.setOperation2("5");
-    this.calcul=this.getOperation2();
-  }
-  console.log(this.calcul);
-}
-
-  function6(){
-    if(this.operation=="")
-      {
-        this.setOperation1("6");
-        this.calcul=this.getOperation1();
-      }
-      else {
-        this.setOperation2("6");
-        this.calcul=this.getOperation2();
-      }
-      console.log(this.calcul);
-  }
-  
-  function7(){
-    if(this.operation=="")
-      {
-        this.setOperation1("7");
-        this.calcul=this.getOperation1();
-      }
-      else {
-        this.setOperation2("7");
-        this.calcul=this.getOperation2();
-      }
-  
-      console.log(this.calcul);
-  }
-
-  function8(){
-    if(this.operation=="")
-      {
-        this.setOperation1("8");
-        this.calcul=this.getOperation1();
-      }
-      else {
-        this.setOperation2("8");
-        this.calcul=this.getOperation2();
-      }
-      console.log(this.calcul);
-  }
-
-  function9(){
-    if(this.operation=="")
-      {
-        this.setOperation1("9");
-        this.calcul=this.getOperation1();
-      }
-      else {
-        this.setOperation2("9");
-        this.calcul=this.getOperation2();
-      }
-      console.log(this.calcul);
-  }
-
-  function0(){
-    if(this.operation=="")
-      {
-        this.setOperation1("0");
-        this.calcul=this.getOperation1();
-      }
-      else {
-        this.setOperation2("0");
-        this.calcul=this.getOperation2();
-      }
-      console.log(this.calcul);
-  }
-
-  decimal(){
-    if(this.operation=="")
-      {
-        if(this.op1=="")
-          {
-            this.setOperation1("0");
-          }
-          this.setOperation1(".");
-          //this.op1+=".";
-          this.calcul=this.getOperation1();
-
-          
-      }
-    else {
-      if(this.op2=="")
-          {
-            this.setOperation2("0");
-          }
-          this.setOperation2(".");
-          //this.op2+="."
-          this.calcul=this.calcul+this.getOperation2();
-    }
-  }
-
-  clear(){
-    this.calcul="";
-    this.op1="";
-    this.op2="";
-    this.operation="";
-    this.res=0;
-    this.i=0,this.j=0;
-  }
-
-  egal(){
-     this.j=parseFloat(this.getOperation2());
-      this.i=parseFloat(this.getOperation1());
-    if(this.operation == "+") {
-      this.res=this.i+this.j;
-    }
-
-    if(this.operation == "*") {
-       this.res=this.i*this.j;
-    }
-
-    if(this.operation == "/") {
-      try {
-        this.res=this.i/this.j;
-      } catch (error) {
-        this.calcul="SYNTAXE ERROR";
-      }
-       
-    }
-
-    if(this.operation == "-") {
-      this.res=this.i-this.j;
-    }
-    if(this.operation == "")  this.res=this.i;
-
-    this.calcul=this.res.toString();
-    console.log(this.res);
-    console.log(this.i);
-  }
-
-  sub(){
-    /*this.i = parseFloat(this.getOperation1());
-    this.j=parseFloat(this.getOperation2());
-    */this.operation="-";
-    //this.res=parseFloat(this.getOperation1());
-    this.calcul="";
-  }
-
-  add(){
-    /*this.i = parseFloat(this.getOperation1());
-    this.j=parseFloat(this.getOperation2());
-    */this.operation="+"
-    //this.res=parseFloat(this.getOperation1());
-    this.calcul="";
-  }
-
-  produit(){
-    /*this.i = parseFloat(this.getOperation1());
-    this.j=parseFloat(this.getOperation2());
-    */this.operation="*"
-    //this.res=parseFloat(this.getOperation1());
-    this.calcul="";
-  }
-
-  div(){
-    this.operation="/";
-    this.calcul="";
+ 
+ 
   }
 }
